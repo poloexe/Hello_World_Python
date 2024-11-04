6f44014ef5adb928bfd7b234d490224cb8b4ed9e
Baruch Isemin <baruchefiok@gmail.com>
2024-11-04 09:47:17 +0100

    Add hello world script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hello_World_Python
+
+This is a simple python script to print "Hello, World!" on the console
+
---
+++ python.py
@@ -0,0 +1,6 @@
+# Name: Isemin Baruch Efiok
+# Matric: 23/11951
+# Dept: Cyber Security
+# 200 Level
+
+print("Hello, World!")
